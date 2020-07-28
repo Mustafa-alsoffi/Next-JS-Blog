37df77d6037b033a948252c64056f02b53a40994
Mustafa Alsoffi <alsoffimustafa@gmail.com>
2020-07-29 00:12:18 +0800

    Add quotes to the date line in an .md file so it is read as string

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,46 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import Alert from '../components/alert'
-import utilStyles from '../styles/utils.module.css'
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Alert from '../components/alert';
 import React, {useState} from 'react';
-import { getSortedPostsData } from '../lib/posts'
+import { getSortedPostsData } from '../lib/posts';
 
-
-
-export default function Home({allPostsData}) {
-
-
+export default function Home({ allPostsData }) {
   const [bol, setBol] = useState(false);
-
   return (
     <Layout home>
-      <Alert type={bol ? 'success' : 'error'}>
-      <Head>…</Head>
-      <section className={utilStyles.headingMd}>…</section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-         {
-        allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
-      </ Alert>
-      <div>
-            <button style={{backgroundColor: 'gray', width: '40px', height: '40px'}} onClick={() => setBol(!bol)}></button>
-      </div>
+    <Alert type={bol ? 'success' : 'error'}>
+    <Head>…</Head>
+    <section className={utilStyles.headingMd}>…</section>
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <h2 className={utilStyles.headingLg}>Blog</h2>
+      <ul className={utilStyles.list}>
+       {
+      allPostsData.map(({ id, date, title }) => (
+          <li className={utilStyles.listItem} key={id}>
+            {title}
+            <br />
+            {id}
+            <br />
+            {date}
+          </li>
+        ))}
+      </ul>
+    </section>
+    </ Alert>
+    <div>
+          <button style={{backgroundColor: 'gray', width: '40px', height: '40px'}} onClick={() => setBol(!bol)}></button>
+    </div>
 
-    </Layout>
+  </Layout>
   )
 }
 
 export async function getStaticProps() {
-
-  const allPostsData = getSortedPostsData();
-  
+  const allPostsData = getSortedPostsData()
   return {
     props: {
       allPostsData
     }
- 
   }
 }
